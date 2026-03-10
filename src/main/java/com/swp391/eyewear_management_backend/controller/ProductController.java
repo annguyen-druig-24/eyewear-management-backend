23689e5e2f42e4083c9e43083042e86d542fd015
Vu Nguyen Duc Huy <soobin205304@gmail.com>
2026-03-10 21:18:55 +0700

    feat: api for admin to get all product isActive and noActive version 2

--- src/main/java/com/swp391/eyewear_management_backend/controller/ProductController.java
+++ src/main/java/com/swp391/eyewear_management_backend/controller/ProductController.java
@@ -32,6 +32,7 @@ public class ProductController {
     }
 
     @GetMapping("/admin/search")
+    @PreAuthorize("hasAnyAuthority('ROLE_SALES STAFF','ROLE_ADMIN','ROLE_MANAGER')")
     public ResponseEntity<List<ProductResponse>> searchProductsAdmin(
             @RequestParam(required = false) String name,
             @RequestParam(required = false) Double minPrice,
