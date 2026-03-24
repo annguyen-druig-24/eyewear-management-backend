1406fa03406651d69e53f6f83addb22f41da552a
Vu Nguyen Duc Huy <soobin205304@gmail.com>
2026-03-25 00:09:16 +0700

    fix: api to create exchange more faster

--- src/main/java/com/swp391/eyewear_management_backend/repository/ReturnExchangeRepo.java
+++ src/main/java/com/swp391/eyewear_management_backend/repository/ReturnExchangeRepo.java
@@ -50,6 +50,12 @@ public interface ReturnExchangeRepo extends JpaRepository<ReturnExchange, Long> 
             String requestScope
     );
 
+        List<ReturnExchange> findByOrder_OrderIDAndReturnTypeIgnoreCaseAndStatusIn(
+            Long orderId,
+            String returnType,
+            List<String> statuses
+        );
+
     @Query(value = """
         SELECT re.Return_Exchange_ID AS returnExchangeId,
                re.Return_Code AS returnCode,
